dd1a805761f54870745dba652962b665d6ffab33
Jesse Kinser <email": null>
2016-07-06 00:30:39 -0700

    swipe mostly working problem with picture loading on startup

--- www/js/controllers/SectionsItemsController.js
+++ www/js/controllers/SectionsItemsController.js
@@ -1,6 +1,21 @@
 neverNude.controller('SectionsItemsController', ['$scope', 'sectionsitems', function($scope, sectionsitems) {
   sectionsitems.get({ id: 1 }, function(data) {
     $scope.items = data.items;
-    $scope.pictures = data.thumb_image_urls;
+    $scope.accerPictures = data.thumb_image_urls;
+  });
+
+  sectionsitems.get({ id: 2 }, function(data) {
+    $scope.items = data.items;
+    $scope.topsPictures = data.thumb_image_urls;
+  });
+
+  sectionsitems.get({ id: 3 }, function(data) {
+    $scope.items = data.items;
+    $scope.bottomsPictures = data.thumb_image_urls;
+  });
+
+  sectionsitems.get({ id: 4 }, function(data) {
+    $scope.items = data.items;
+    $scope.footPictures = data.thumb_image_urls;
   });
 }]);
