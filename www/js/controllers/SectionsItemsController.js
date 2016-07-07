cd134e3adc1967ee7464adc295573b66061a88dd
Danny Pham <email": null>
2016-07-07 14:15:16 -0700

    Save outfit now works

--- www/js/controllers/SectionsItemsController.js
+++ www/js/controllers/SectionsItemsController.js
@@ -1,35 +1,51 @@
-neverNude.controller('SectionsItemsController', ['$scope', '$ionicSlideBoxDelegate', 'sectionsitems', function($scope, $ionicSlideBoxDelegate, sectionsitems) {
+neverNude.controller('SectionsItemsController', ['$scope', '$ionicSlideBoxDelegate', 'sectionsitems', '$http', function($scope, $ionicSlideBoxDelegate, sectionsitems, $http) {
 
   $scope.updateSlider = function () {
     $ionicSlideBoxDelegate.update();
     $ionicSlideBoxDelegate.loop(true);
-    $scope.indexAccer = $ionicSlideBoxDelegate.$getByHandle('accer').currentIndex();
-    $scope.indexTops = $ionicSlideBoxDelegate.$getByHandle('tops').currentIndex();
-    $scope.indexBottoms = $ionicSlideBoxDelegate.$getByHandle('bottoms').currentIndex();
-    $scope.indexFootwear = $ionicSlideBoxDelegate.$getByHandle('footwear').currentIndex();
-  }
+  };
 
   sectionsitems.get({ id: 1 }, function(data) {
     $scope.items = data.items;
     $scope.accerPictures = data.med_image_urls;
     $scope.accerLrgPictures = data.lrg_image_urls;
+    accessories = data.items;
   });
 
   sectionsitems.get({ id: 2 }, function(data) {
     $scope.items = data.items;
     $scope.topsPictures = data.med_image_urls;
     $scope.topsLrgPictures = data.lrg_image_urls;
+    tops = data.items;
   });
 
   sectionsitems.get({ id: 3 }, function(data) {
     $scope.items = data.items;
     $scope.bottomsPictures = data.med_image_urls;
     $scope.bottomsLrgPictures = data.lrg_image_urls;
+    bottoms = data.items;
   });
 
   sectionsitems.get({ id: 4 }, function(data) {
     $scope.items = data.items;
     $scope.footPictures = data.med_image_urls;
     $scope.footLrgPictures = data.lrg_image_urls;
+    foot = data.items;
   });
+
+  $scope.saveOutfit = function() {
+    var accerId = accessories[$ionicSlideBoxDelegate.$getByHandle('accer').currentIndex()].id;
+    var topsId = tops[$ionicSlideBoxDelegate.$getByHandle('tops').currentIndex()].id;
+    var bottomsId = bottoms[$ionicSlideBoxDelegate.$getByHandle('bottoms').currentIndex()].id;
+    var footId = foot[$ionicSlideBoxDelegate.$getByHandle('footwear').currentIndex()].id;
+
+    outfit = JSON.stringify({outfit: {user_id: 1, accer_id: accerId, tops_id: topsId, bottoms_id: bottomsId, foot_id: footId}})
+
+    $http.post('https://nevernude.herokuapp.com/outfits', outfit, {
+      headers: {'Content-Type': 'application/json'}
+    })
+    .success(function(data) {
+      alert('Outfit saved.');
+    })
+  };
 }]);
