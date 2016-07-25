197b83ca2c308953d58af3f5f16d1cf754af4488
whatthemandy <amcdorman@gmail.com>
2016-07-25 12:37:30 -0700

    Update test name

--- www/js/controllers/TestsController.js
+++ www/js/controllers/TestsController.js
@@ -1,31 +1,31 @@
 neverNude.controller('TestsController', ['$scope', function($scope) {
-  $scope.test = 'FUCK';
-  $scope.pictures = [ 
-  { 
+  $scope.test = 'TEST';
+  $scope.pictures = [
+  {
     name: 'closet1',
     cover: 'img/closet1.jpg'
-  }, 
-  { 
+  },
+  {
     name: 'closet2',
     cover: 'img/closet2.jpg'
   },
-  { 
+  {
     name: 'closet3',
     cover: 'img/closet3.jpg'
   },
-  { 
+  {
     name: 'closet4',
     cover: 'img/closet4.jpg'
   },
-  { 
+  {
     name: 'closet5',
     cover: 'img/closet5.jpg'
   },
-  { 
+  {
     name: 'closet6',
     cover: 'img/closet6.jpg'
   },
-  { 
+  {
     name: 'closet7',
     cover: 'img/closet7.jpg'
   },
